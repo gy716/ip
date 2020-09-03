498bb8f2669540d8677089d6e4f86a215e9edb12
Geng Yu <e0496102@u.nus.edu>
2020-09-03 17:13:01 +0800

    coding quality 01

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -13,6 +13,7 @@ public class Duke {
     }
 
     void processCommands(){
+
         int i = 0;
 
         while(true) {
@@ -22,56 +23,71 @@ public class Duke {
 
             if (commands.equals("list")) {
 
-                System.out.println("    Here are the list:");
-
-                if (tasks[0] != null) {
-
-                    for (int k = 0; k < i; k++) {
-                        tasks[k].list();
-                    }
-
-                } else {
-                    System.out.println();
-                }
+                processList(i);
 
             } else if (commands.equals("exit")) {
 
-                System.out.println("    Good Bye! Hope to see you again");
+                System.out.println("\tGood Bye! Hope to see you again");
                 break;
 
             } else if (words[0].equals("todo")) {
 
-                commands = commands.replaceAll("todo ", "");
-                tasks[i] = new Task(commands, i + 1);
-                tasks[i].showAdded();
+                processTodo(i);
                 i++;
 
             } else if (words[0].equals("deadline")) {
 
-                String by = commands.substring(commands.indexOf("/by") + "/by ".length());
-                commands = commands.substring(9, commands.indexOf("/by") - 1);
-                tasks[i] = new Deadline(commands, i + 1, by);
-                tasks[i].showAdded();
+                processDeadline(i);
                 i++;
 
             } else if (words[0].equals("event")) {
 
-                String at = commands.substring(commands.indexOf("/at") + "/at ".length());
-                commands = commands.substring(6, commands.indexOf("/at") - 1);
-                tasks[i] = new Event(commands, i + 1, at);
-                tasks[i].showAdded();
+                processEvent(i);
                 i++;
 
             } else if(words[0].equals("done")){
 
-                int taskIndex = Integer.parseInt(words[1]);
-                tasks[taskIndex-1].isDone = true;
-                System.out.println("    Nice! I've marked this work as done:");
-                System.out.println("        [\u2713] "+tasks[taskIndex-1].description);
+                processDone(i, words);
 
             }
 
         }
+    }
+
+    void processList(int i) {
+        System.out.println("\tHere are the list:");
+        if (tasks[0] != null) {
+            for (int k = 0; k < i; k++) {
+                tasks[k].list();
+            }
+        }
+    }
+
+    void processTodo(int i) {
+        commands = commands.replaceAll("todo ", "");
+        tasks[i] = new Task(commands, i + 1);
+        tasks[i].showAdded();
+    }
+
+    void processDeadline(int i) {
+        String by = commands.substring(commands.indexOf("/by") + 4);
+        commands = commands.substring(9, commands.indexOf("/by") - 1);
+        tasks[i] = new Deadline(commands, i + 1, by);
+        tasks[i].showAdded();
+    }
+
+    void processEvent(int i) {
+        String at = commands.substring(commands.indexOf("/at") + 4);
+        commands = commands.substring(6, commands.indexOf("/at") - 1);
+        tasks[i] = new Event(commands, i + 1, at);
+        tasks[i].showAdded();
+    }
+
+    void processDone(int i, String[] words) {
+        int taskIndex = Integer.parseInt(words[1]);
+        tasks[taskIndex-1].isDone = true;
+        System.out.println("\tNice! I've marked this work as done:");
+        System.out.println("\t\t[\u2713] "+tasks[taskIndex-1].description);
     }
 
 
