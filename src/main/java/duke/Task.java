0fa22cac79806df55c53786d1b0fc6427bbe7a85
Geng Yu <e0496102@u.nus.edu>
2020-09-16 07:14:23 +0800

    level-6 v1

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -2,23 +2,28 @@ package duke;
 public class Task {
     protected String description;
     protected boolean isDone;
-    protected int taskNumber;
 
-    public Task(String description, int taskNumber) {
+
+    public Task(String description) {
         this.description = description;
         this.isDone = false;
-        this.taskNumber = taskNumber;
+
     }
 
     public void showAdded() {
         System.out.println("\tGot it! I've added this task:");
         System.out.println("\t\t[T]["+getStatusIcon()+"] "+description);
-        System.out.println("\tNow you have "+taskNumber+" tasks in the list.");
+    }
+
+    public void showDeleted() {
+        System.out.println("\tNoted! I've removed this task:");
+        System.out.println("\t\t[T]["+getStatusIcon()+"] "+description);
     }
 
     public void printTask() {
-        System.out.println("\t"+taskNumber+".[T]["+getStatusIcon()+"] "+description);
+        System.out.println("[T]"+"["+getStatusIcon()+"]"+description);
     }
+
 
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
